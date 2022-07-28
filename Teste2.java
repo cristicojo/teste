c9dcc9fe240238e8864d5fcf567c7493bd4aa3a9
Cristi Cojocaru <crii_stii@yahoo.com>
2022-07-28 17:02:49 +0300

    Update Teste2.java

--- Teste2.java
+++ Teste2.java
@@ -1,30 +1,14 @@
 // aabbbNnnncca --> a2b3n4c2a1
 public class Teste4 {
 
-	public String convert(String input) {
+	ppublic String convert(String input) {
 
-		int S = 0;
-		String ham = "";
-		char[] charArray = input.toCharArray();
+		String[] out = input.toLowerCase().split("(?<=(.))(?!\\1)");
+		StringBuilder ham = new StringBuilder();
 
+		Arrays.stream(out).forEach(i -> ham.append((i.charAt(0))).append(i.length()));
 
-		for (int i = 0; i < charArray.length; i++) {
-			ham = ham + String.valueOf(charArray[i]).toLowerCase();
-
-			if (i + 1 < charArray.length && !String.valueOf(charArray[i]).equalsIgnoreCase(String.valueOf(charArray[i + 1]))) {
-				ham = ham + ",";
-			}
-		}
-
-		String[] arrOfStr = ham.split(",");
-
-		ham = "";
-		for (String s : arrOfStr) {
-			ham = ham + s.charAt(0) + s.length();
-		}
-
-		return ham;
-
+		return ham.toString();
 	}
 
 	public static void main(String[] args) {
