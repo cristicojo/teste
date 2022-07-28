66a3a9be2ccdf73c0e57083eb8905d7c3b273364
Cristi Cojocaru <crii_stii@yahoo.com>
2022-07-28 16:01:09 +0300

    Update Teste1.java

--- Teste1.java
+++ Teste1.java
@@ -4,74 +4,22 @@ import java.util.stream.Collectors;
 // print the smallest int positive number != 0 from an array that is not present in the array
 public class Teste2 {
 
-	public Teste2() {
-	}
+	public int find(Integer[] a) {
 
+		List<Integer> list = Arrays.asList(a);
+		Set<Integer> set = new TreeSet<>(list);
 
-	public int andrei(int[] p) {
-
-		Set<Integer> s = Arrays.stream(p).boxed().collect(Collectors.toCollection(TreeSet::new));
-
-		for (int i = 1; i <= s.size(); i++) {
-
-			if (!(s.contains(i))) {
-				return i;
-			}
-		}
-
-		return 0;
-	}
-
-	public int[] citire() {
-
-		Scanner s = new Scanner(System.in);
-		System.out.print("Dati nr de sloturi = ");
-
-		int n = s.nextInt();
-		int[] a = new int[n];
-
-		for (int i = 0; i < a.length; i++) {
-			System.out.print("a[" + i + "] = ");
-			a[i] = s.nextInt();
-		}
-
-		return a;
-	}
-
-	public void afisare(int[] a) {
-
-		Arrays.stream(a).forEach(elem -> System.out.print(elem+" "));
-		System.out.println();
-	}
-
-	public void eroare(int[] m) {
-
-		Set<Integer> s = Arrays.stream(m).boxed().collect(Collectors.toCollection(TreeSet::new));
-		List<Integer> list = new ArrayList<>();
-
-		for (int i = 1; i <= s.size(); i++) {
-			list.add(i);
-		}
-
-		if (s.containsAll(list)) {
-			System.out.println("Please give numbers > 1");
-			int[] b = citire();
-			afisare(b);
-			eroare(b);
-			andrei(b);
-		}
+		return Stream.iterate(0, i -> i + 1)
+				.filter(i -> !set.contains(i))
+				.findFirst()
+				.get();
 
 	}
 
 
 	public static void main(String[] args) {
 
-		Teste2 cristi = new Teste2();
-
-		int[] b = cristi.citire();
-		cristi.afisare(b);
-		cristi.eroare(b);
-		System.out.println(cristi.andrei(b));
-
+		Integer[] a = new Integer[]{3, 9, 0, 4, 1, 3};
+		System.out.println(new Teste2().find(a));
 	}
 }
